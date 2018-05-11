25f87cc6699f463a55479a9ba64a94cdb5ba3244
Jean A. Senellart <jean.senellart@systrangroup.com>
2018-05-11 09:34:50 +0200

    validate model4

--- test/data/model4.lua
+++ test/data/model4.lua
@@ -5,4 +5,6 @@ mlp:add(nn.Linear(10, 25)) -- Linear module (10 inputs, 25 hidden units)
 mlp:add(nn.Tanh())         -- apply hyperbolic tangent transfer function on each hidden units
 mlp:add(nn.Linear(25, 1))  -- Linear module (25 inputs, 1 output)
 
+mlp:forward(torch.randn(10))
+
 torch.save("model4.t7", mlp)
